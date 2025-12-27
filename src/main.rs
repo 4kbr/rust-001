bba06b2e45e139c1a09c8f4d9b15109cbeb0dcb9
4kbr <>
2025-12-27 18:34:46 +0700

    sampai data type

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+fn main() {
+    // ```cargo run```
+    //  ```cargo run``` untuk menjalankan rust dengan membuat file bin di folder target/debug
+    //   ```cargo build --release``` untuk membuat bin
+    println!("Hello, world!");
+    // let x:i32 = 1;
+}
+
+// ```cargo test hello_test -- --exact```
+// ```cargo test hello_test -- --exact --nocapture```
+#[test]
+fn hello_test() {
+    println!("hello_test: called")
+}
+
+// variable bersifat immutable (tidak bisa diubah) secara default
+#[test]
+fn test_variable() {
+    let name = "My Name";
+    // name = "sada"; // error
+    println!("hello {}", name);
+}
+
+// variable dengan kata kunci key mut = bisa diubah / assign ulang
+#[test]
+fn test_mutable() {
+    let mut mutable_name = "My Name";
+    println!("hello before edit {}", mutable_name);
+
+    mutable_name = "Antohr";
+    println!("hello after edit {}", mutable_name);
+}
+
+// rust bersofat static typing, jadi variable yang sudah diassign dengan type data tertentu tidak bisa diubah kembali type datanya
+#[test]
+fn static_typing() {
+    // let mut name = "My Name";
+    let mut name = "My Name";
+    println!("hello {}", name);
+
+    // name = 10; // ini gak bisa
+    name = "10";
+    println!("hello {}", name);
+}
+
+#[test]
+fn data_type() {
+    // setiap nilai di rust punya type data, secara garis besar type data di rust dibagi 2 yaitu "scalar" dan "compound"
+    // scalar type yang single value
+    // seperti `interger`, `float`, `boolean`, dan `char`.
+    //  misal `let my_age = 10`
+
+    // sementara compound type mempresentasikan beberapa value (mirip seperti object dan array di javascript),
+    // yaitu: `tuple` dan `array`
+    // let first_array:[i32;4] = [1,23,4,2];
+
+    // secara default rust akan mendeteksi type data yang kita pakai, tanpa perlu diketik sendiri, tapi juga bisa diketik sendiri
+
+    // misal explicit
+    let age: i32 = 20;
+    println!("umurnya adalah: {}", age); // bisa seperti ini
+    // println!("umurnya adalah: {age}");
+
+    // misal non explicit, rust auto assign
+    let age_no_explicit = 20;
+    println!("umurnya adalah: {age_no_explicit}");
+}
+
