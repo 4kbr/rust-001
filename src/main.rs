7f623ba59308f54f2187916835be5d7350d06506
4kbr <>
2025-12-29 22:31:27 +0700

    Iterators

--- src/main.rs
+++ src/main.rs
@@ -2752,3 +2752,57 @@ fn test_btree_map() {
     // country: Indonesia
     // name: Olive
 }
+
+/* Iterator
+- Rust memiliki module Iterator, yang digunakan sebagai mekanisme untuk melakukan operasi terhadap urutan dari data
+- Semua tipe data yang multiple seperti Array, Slice dan Collection memiliki fitur Iterator
+- Dengan menggunakan Iterator, secara otomatis kita bisa melakukan iterasi data menggunakan For Loop terhadap value tersebut
+- https://doc.rust-lang.org/std/iter/index.html
+
+sebenarnya di setiap perulangan seperti
+for v in array{...} ini di aslinya pakai method .into_iter()
+for v in array.into_iter() {...}
+
+# Iterator Method
+- Iterator memiliki banyak sekali method yang bisa kita gunakan untuk memanipulasi data
+- Kebanyakan method di Iterator itu menggunakan Closure sebagai parameternya
+- Ada banyak sekali method yang disediakan, dan disarankan untuk membaca dokumentasinya secara langsung
+- https://doc.rust-lang.org/std/iter/trait.Iterator.html
+
+
+*/
+
+#[test]
+fn test_iterator() {
+    let array: [i32; 5] = [1, 2, 3, 4, 5];
+    // ini cara manual for loop dengan iter
+    let mut iterator = array.iter();
+    // untuk pakai iterator.next() maka iterator nya perlu jadi mutable alias mut, jika tidak maka akan error
+    while let Some(value) = iterator.next() {
+        println!("{}", value);
+    }
+
+    // ini dengan perulanga biasa
+    // for value in iterator {
+    for value in iterator.into_iter() {
+        println!("{}", value);
+    }
+}
+
+#[test]
+fn test_iterator_method() {
+    let vector: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    println!("Vector: {:?}", vector);
+
+    let sum: i32 = vector.iter().sum();
+    println!("Sum: {}", sum);
+
+    let count: usize = vector.iter().count();
+    println!("Count: {}", count);
+
+    let doubled: Vec<i32> = vector.iter().map(|x| x * 2).collect();
+    println!("Doubled: {:?}", doubled);
+
+    let odd: Vec<&i32> = vector.iter().filter(|x| *x % 2 != 0).collect();
+    println!("Odd: {:?}", odd);
+}
