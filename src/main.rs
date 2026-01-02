6f5c634e36afcf68c7e13432d7de2fdd19856874
4kbr <>
2026-01-03 05:57:57 +0700

    handle error

--- src/main.rs
+++ src/main.rs
@@ -2806,3 +2806,123 @@ fn test_iterator_method() {
     let odd: Vec<&i32> = vector.iter().filter(|x| *x % 2 != 0).collect();
     println!("Odd: {:?}", odd);
 }
+
+/* Error Handling
+- Error merupakan hal yang sudah biasa dalam pembuatan aplikasi
+- Hampir semua bahasa pemrograman memiliki fitur untuk Error Handling, termasuk Rust
+- Rust membagi error menjadi dua jenis recoverable (dapat dipulihkan) dan unrecoverable (tidak dapat dipulihkan)
+- Rust tidak punya tipe data Exception, seperti di Java, PHP, JavaScript
+- Rust menggunakan pendekatan lain untuk Error Handling
+
+# Unrecoverable Error
+- Jika terdapat jenis error yang menurut kita tidak bisa dipulihkan, maka kita bisa gunakan jenis Unrecoverable Error
+- Rust menggunakan macro panic! untuk melakukan ini
+- Misal saat aplikasi yang kita buat berjalan, ternyata tidak ada konfigurasi untuk terkoneksi ke database.
+Tidak ada gunanya mengecek ulang pada kasus ini misalnya, lebih baik matikan aplikasi dan sebutkan error bahwa konfigurasi tidak ada. Pada kasus ini, kita bisa gunakan Unrecoverable Error
+- Beberapa hal di Rust juga menggunakan Unrecoverable Error, contoh ketika mengakses index di array / vector diluar jangkauan index nya
+
+# Recoverable Error
+- Seperti yang dijelaskan di awal, Rust tidak memiliki tipe data Exception. Lantas bagaimana untuk jenis Recoverable Error? Sama seperti Enum Option, Rust menyediakan Enum Result untuk ini
+- Jadi ketika misal kita membuat function yang bisa mengembalikan sukses atau gagal, kita bisa buat function dengan return value Enum Result
+- Enum Result hanya memiliki dua nilai, Ok(T) dan Err(E)
+- https://doc.rust-lang.org/std/result/index.html
+- https://doc.rust-lang.org/std/result/enum.Result.html
+
+
+# ? Operator
+- Saat menggunakan Recoverable Error, kadang kita sering memanggil beberapa jenis function yang menghasilkan Result,
+lalu ingin mengecek, jika Err maka kita ingin langsung mengembalikan error itu secara langsung
+- Jika melakukan manual menggunakan Pattern Matching, kadang menyulitkan
+- Kita bisa menggunakan ? Operator, yang secara otomatis bisa mengembalikan Result jika memang Err
+*/
+
+fn connect_database(host: Option<String>) {
+    match host {
+        Some(host) => {
+            println!("Connecting to database at {}", host);
+        }
+        None => {
+            panic!("No database host provided");
+        }
+    }
+}
+
+#[test]
+fn test_connect_db() {
+    connect_database(Some(String::from("Ini DB"))); // ini berhasil
+    connect_database(None); // ini error panic
+}
+
+fn connect_cache(host: Option<String>) -> Result<String, String> {
+    match host {
+        Some(host) => Ok(host),
+        None => Err("No cache host provided".to_string()),
+    }
+}
+
+#[test]
+fn test_recoverable_error() {
+    // let cache = connect_cache(None);
+    let cache = connect_cache(Some(String::from("value")));
+    // tidak ada try catch didalam rust, jadi pakainya enum result
+    match cache {
+        Ok(host) => {
+            println!("Connected to cache at {}", host);
+        }
+        Err(err) => {
+            println!("Error connecting to cache: {}", err);
+        }
+    }
+}
+
+fn connect_email(host: Option<String>) -> Result<String, String> {
+    match host {
+        Some(host) => Ok(host),
+        None => Err("No email host provided".to_string()),
+    }
+}
+
+fn connect_application(host: Option<String>) -> Result<String, String> {
+    // tanpa operator ?
+    // let cache_result = connect_cache(host.clone());
+    // match cache_result {
+    //     Ok(_) => {}
+    //     Err(err) => {
+    //         return Err(err);
+    //     }
+    // }
+    // let email_result = connect_email(host.clone());
+    // match email_result {
+    //     Ok(_) => {}
+    //     Err(err) => {
+    //         return Err(err);
+    //     }
+    // }
+    // Ok("Connected to application".to_string())
+
+    // dengan operator ?
+    connect_cache(host.clone())?;
+    connect_email(host.clone())?;
+    Ok("Connected to application".to_string())
+}
+#[test]
+fn test_connect_app() {
+    let result = connect_application(Some(String::from("Cameron")));
+    // let result = connect_application(None);
+    match result {
+        Ok(host) => println!("success connect with message: {}", host),
+        Err(error) => println!("error with message : {}", error),
+    }
+
+    println!("connected to db")
+}
+
+#[test]
+fn test_application_error() {
+    // let result = connect_application(Some(String::from("Cameron")));
+    let result = connect_application(None);
+    match result {
+        Ok(host) => println!("success connect with message: {}", host),
+        Err(error) => println!("error with message : {}", error),
+    }
+}
