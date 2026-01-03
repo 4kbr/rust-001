047a2116a189bc1723d694575409c131d6440add
4kbr <>
2026-01-03 13:50:04 +0700

    test integration

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn say_hello(name: &str) -> String {
+    format!("Hello {}", name)
+}
+pub fn say_hello_to_everyone() -> String {
+    String::from("Hello All")
+}
+pub fn say_good_bye(name: &str) -> String {
+    format!("Good Bye {}", name)
+}
+pub fn say_good_bye_to_everyone() -> String {
+    String::from("Good Bye All")
+}
+ 
---
+++ tests/say_hello.rs
@@ -0,0 +1,27 @@
+// use my_crate::say_hello; // Ganti `my_crate` dengan nama crate Anda
+
+use unit_test::say_hello;
+
+#[test]
+fn test_hello_success() {
+    let result = say_hello("Alice");
+    assert_eq!(result, "Hello Alice");
+}
+
+#[test]
+fn test_hello_empty_name() {
+    let result = say_hello("");
+    assert_eq!(result, "Hello ");
+}
+
+// #[test]
+// fn test_hello_null_name() {
+//     let result = say_hello(None);
+//     assert_eq!(result, "Hello World");
+// }
+
+// #[test]
+// #[should_panic(expected = "Name cannot be numeric")]
+// fn test_hello_numeric_name() {
+//     say_hello("123");
+// }
