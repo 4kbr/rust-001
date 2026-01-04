02ab46ba3442af3c8471687ac57a7fe866246273
4kbr <igyutubfesbuk@gmail.com>
2026-01-05 05:35:13 +0700

    environment variable

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,27 @@ fn test_config() {
 
     assert!(config.get_string("APP_NAME").is_err());
 }
+
+// ## Environment variable
+#[test]
+fn test_config_env() {
+    use std::env::set_var;
+    unsafe {
+        set_var("DB_HOST", "localhost");
+        set_var("DB_PORT", "5432");
+        set_var("DB_USER", "eko");
+        set_var("DB_PASSWORD", "rahasia");
+    }
+
+    let config = Config::builder()
+        .add_source(config::Environment::default().convert_case(config::Case::Snake))
+        .build()
+        .unwrap();
+
+    println!("db host: {}", config.get_string("db_host").unwrap());
+    assert_eq!(config.get_string("db_host").unwrap(), "localhost");
+    assert_eq!(config.get_int("db_port").unwrap(), 5432);
+
+    assert_eq!(config.get_string("db_user").unwrap(), "eko");
+    assert_eq!(config.get_string("db_password").unwrap(), "rahasia");
+}
