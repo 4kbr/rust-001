d52754e7db7911f40edc9882dfde1471af5b4ce8
4kbr <igyutubfesbuk@gmail.com>
2026-01-05 05:55:34 +0700

    Yaml

--- src/main.rs
+++ src/main.rs
@@ -67,3 +67,24 @@ fn test_json() {
 
     assert_eq!(config.get_string("database.password").unwrap(), "password");
 }
+#[test]
+fn test_yaml() {
+    let config = Config::builder()
+        .add_source(config::File::new(
+            "application.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()
+        .unwrap();
+
+    assert_eq!(config.get_string("name").unwrap(), "My Application");
+    assert_eq!(config.get_string("database.host").unwrap(), "localhost");
+
+    assert_eq!(config.get_int("database.port").unwrap(), 5432);
+
+    assert_eq!(config.get_string("database.name").unwrap(), "my_database");
+
+    assert_eq!(config.get_string("database.username").unwrap(), "user");
+
+    assert_eq!(config.get_string("database.password").unwrap(), "password");
+}
