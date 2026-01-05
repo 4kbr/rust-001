842c8141828fd6f6e6a83632bc9029324ea57b49
4kbr <igyutubfesbuk@gmail.com>
2026-01-05 17:57:55 +0700

    Setup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
+use handlebars::Handlebars;
+
 // ## TEMPLATE
 // pakai library `handlebars`
 // `cargo add handlebars`
 fn main() {
     println!("Hello, world!");
+}
+
+// ## Setup
+#[test]
+fn test_handlebars() {
+    let mut handlebars = handlebars::Handlebars::new();
+
+    handlebars
+        .register_template_string("hello", "Hello, {{name}}")
+        .unwrap();
+
+    handlebars
+        .register_template_string("bye", "Bye, {{name}}")
+        .unwrap();
+
+    let mut data = std::collections::HashMap::new();
+    data.insert("name", "Eko");
+    let rendered = handlebars.render("hello", &data).unwrap();
+    assert_eq!(rendered, "Hello, Eko");
+    let rendered = handlebars.render("bye", &data).unwrap();
+    assert_eq!(rendered, "Bye, Eko");
 }
