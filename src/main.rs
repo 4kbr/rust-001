191611520638f2b37774dca53c4f8bdc27d09da9
4kbr <>
2026-01-05 22:16:11 +0700

    Variable

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,29 @@ use handlebars::Handlebars;
 // pakai library `handlebars`
 // `cargo add handlebars`
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
 }
 
 // ## Setup
 // cargo test --package rust-template --bin rust-template -- test_handlebars --exact --nocapture
 #[test]
 fn test_handlebars() {
-    let mut handlebars = handlebars::Handlebars::new();
+  let mut handlebars = handlebars::Handlebars::new();
 
-    handlebars
-        .register_template_string("hello", "Hello, {{name}}")
-        .unwrap();
+  handlebars
+    .register_template_string("hello", "Hello, {{name}}")
+    .unwrap();
 
-    handlebars
-        .register_template_string("bye", "Bye, {{name}}")
-        .unwrap();
+  handlebars
+    .register_template_string("bye", "Bye, {{name}}")
+    .unwrap();
 
-    let mut data = std::collections::HashMap::new();
-    data.insert("name", "Eko");
-    let rendered = handlebars.render("hello", &data).unwrap();
-    assert_eq!(rendered, "Hello, Eko");
-    let rendered = handlebars.render("bye", &data).unwrap();
-    assert_eq!(rendered, "Bye, Eko");
+  let mut data = std::collections::HashMap::new();
+  data.insert("name", "Eko");
+  let rendered = handlebars.render("hello", &data).unwrap();
+  assert_eq!(rendered, "Hello, Eko");
+  let rendered = handlebars.render("bye", &data).unwrap();
+  assert_eq!(rendered, "Bye, Eko");
 }
 
 // ## Nested Variable
@@ -36,36 +36,34 @@ fn test_handlebars() {
 // cargo test --package rust-template --bin rust-template -- test_nested_variable --exact --nocapture
 #[test]
 fn test_nested_variable() {
-    let mut handlebars = Handlebars::new();
-    handlebars
-        .register_template_string("hello", "Hello, {{person.first_name}} {{person.last_name}}")
-        .unwrap();
-    let mut data = HashMap::new();
-    let mut person = HashMap::new();
-    person.insert("first_name", "Eko");
-    person.insert("last_name", "Khannedy");
-    data.insert("person", person);
-    let rendered = handlebars.render("hello", &data).unwrap();
-    assert_eq!(rendered, "Hello, Eko Khannedy");
+  let mut handlebars = Handlebars::new();
+  handlebars
+    .register_template_string("hello", "Hello, {{person.first_name}} {{person.last_name}}")
+    .unwrap();
+
+  let mut data = HashMap::new();
+  let mut person = HashMap::new();
+
+  person.insert("first_name", "Eko");
+  person.insert("last_name", "Khannedy");
+  data.insert("person", person);
+
+  let rendered = handlebars.render("hello", &data).unwrap();
+  assert_eq!(rendered, "Hello, Eko Khannedy");
 }
 
-// ## escape
+// ## HTML escape
 #[test]
 fn test_html_escape() {
-    let mut handlebars = handlebars::Handlebars::new();
+  let mut handlebars = handlebars::Handlebars::new();
 
-    handlebars
-        .register_template_string("hello", "Hello, {{{name}}}")
-        .unwrap();
+  handlebars
+    .register_template_string("hello", "Hello, {{{name}}}")
+    .unwrap();
 
-    handlebars
-        .register_template_string("bye", "Bye, {{name}}")
-        .unwrap();
+  let mut data = std::collections::HashMap::new();
+  data.insert("name", "<p>Anne</p>");
 
-    let mut data = std::collections::HashMap::new();
-    data.insert("name", "<p>Eko</p>");
-    let rendered = handlebars.render("hello", &data).unwrap();
-    assert_eq!(rendered, "Hello, <p>Eko</p>");
-    let rendered = handlebars.render("bye", &data).unwrap();
-    assert_eq!(rendered, "Bye, Eko");
+  let rendered = handlebars.render("hello", &data).unwrap();
+  assert_eq!(rendered, "Hello, <p>Anne</p>");
 }
