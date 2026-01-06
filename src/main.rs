1f780579e7460114f1dbf8e391a076cb720bfffe
4kbr <>
2026-01-06 22:17:49 +0700

    If use

--- src/main.rs
+++ src/main.rs
@@ -162,3 +162,41 @@ fn test_serde_json() {
   let rendered = handlebars.render("hello", &data).unwrap();
   assert_eq!(rendered.contains("<h1>Hello Eko Ayub</h1>"), true);
 }
+
+#[test]
+fn test_if() {
+  let mut handlebars = Handlebars::new();
+
+  handlebars
+    .register_template_file("blog", "templates/blog.mustache")
+    .unwrap();
+
+  let data = serde_json::json!( {
+    "title": "Belajar rust",
+    "content": "Content rust"
+  });
+
+  let rendered = handlebars.render("blog", &data).unwrap();
+  assert_eq!(rendered.contains("Belajar rust"), true);
+  assert_eq!(rendered.contains("Content rust"), true);
+  assert_eq!(rendered.contains("Anonymous"), true);
+}
+#[test]
+fn test_if_2() {
+  let mut handlebars = Handlebars::new();
+
+  handlebars
+    .register_template_file("blog", "templates/blog.mustache")
+    .unwrap();
+
+  let data = serde_json::json!( {
+    "title": "Belajar rust",
+    "content": "Content rust",
+    "author":"Meiau"
+  });
+
+  let rendered = handlebars.render("blog", &data).unwrap();
+  assert_eq!(rendered.contains("Belajar rust"), true);
+  assert_eq!(rendered.contains("Content rust"), true);
+  assert_eq!(rendered.contains("Meiau"), true);
+}
