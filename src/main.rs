735c69a023bd43123f6e06f8b6692e6a17d848d7
4kbr <igyutubfesbuk@gmail.com>
2026-01-07 15:19:28 +0700

    integration test

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ cargo add an yhow
 */
 
 use axum::{Router, routing::get};
+use axum_test::TestServer;
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -22,4 +23,16 @@ async fn main() {
     println!("Running app on server 0.0.0.0:8100");
     axum::serve(listener, app).await.unwrap();
     println!("after unwrap");
+}
+
+// ## test
+#[tokio::test]
+async fn test_axum() {
+    let app = Router::new().route("/", get(|| async { "Hello Akhi" }));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/").await;
+
+    response.assert_status_ok();
+    response.assert_text("Hello Akhi");
 }
