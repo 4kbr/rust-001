51ab79965d4a57f85efb3766e9d8c7cdbf5b3de9
4kbr <igyutubfesbuk@gmail.com>
2026-01-07 15:00:48 +0700

    init axum

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+// API with axum
+/*
+cargo add tokio --features full
+cargo add tower
+cargo add serde --features derive
+cargo add http
+cargo add axum
+cargo add axum-extra
+cargo add axum-test
+cargo add an yhow
+
+*/
+fn main() {
+    println!("Hello, world!");
+}
